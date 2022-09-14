028d656e7462797dbe310e9bbca9456d19b06767
Leonardo Nicolas <leonardon397@hotmail.com>
2022-09-14 18:48:58 -0300

    Fundamentos de Java
    Meu primeiro programa em java, criando uma espécie de 'Olá mundo' usando o atalho do eclipse e digitando o comando de saída por extenso

---
+++ src/fundamentos/PrimeiroPrograma.java
@@ -0,0 +1,10 @@
+package fundamentos;
+
+public class PrimeiroPrograma {
+
+	public static void main(String[] args) {
+		System.out.println("Meu primeiro programa em Java pt #01!!!");
+		System.out.println("Meu primeiro programa em Java pt #02!!!");
+
+	}
+}
