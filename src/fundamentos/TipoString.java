404e1833ca0a7332694348b60ec525ac68cb0d13
Leonardo Nicolas <leonardon397@hotmail.com>
2022-09-14 19:01:32 -0300

    Fundamentos de Java
    Aprendendo mais sobre o tipo de dados String e a manipulação de palavras e frases...

---
+++ src/fundamentos/TipoString.java
@@ -0,0 +1,11 @@
+package fundamentos;
+
+public class TipoString {
+	public static void main(String[] args) {
+		System.out.println("Olá pessoal".charAt(0));
+		
+		String s = "Boa tarde";
+		System.out.println(s.concat("!!!"));
+		System.out.println(s + "!!!");
+	}
+}
