b3535c36ff651dc89e7a36aa9a660bb60c7adc29
Leonardo Nicolas <leonardon397@hotmail.com>
2022-09-14 19:05:19 -0300

    Fundamentos de Java
    Este arquivo foi criado e editado pelo editor de texto VIM (minha particularidade com editores de texto, mas eu poderia ter usado o VSCode também) e, com este arquivo foi ensinado a como executar o código sem depender do Eclipse/IntelliJ, utilizando um terminal e rodando o comando 'javac' (Como alternativas a NÃO FICAR DEPENDENTE de IDE's)...

---
+++ src/fundamentos/Console.java
@@ -0,0 +1,14 @@
+package fundamentos;
+
+import java.util.Scanner;
+
+public class Console {
+
+	public static void main(String[] args) {
+		Scanner entrada = new Scanner(System.in);
+		
+		entrada.nextLine();
+		
+		entrada.close();
+	}
+}
